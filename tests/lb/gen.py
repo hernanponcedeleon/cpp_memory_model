08474455bc914b160b0602a58386cfdbd71618aa
Gonzalo Brito Gadeschi <gonzalob@nvidia.com>
2025-10-12 23:50:06 +0200

    Cleanup lb tests

--- tests/lb/gen.py
+++ tests/lb/gen.py
@@ -18,6 +18,9 @@ P1 (int* x, int* y) {{
 
 {cond}"""
 
+ok_cond = '~exists(0:a=1)'
+racy_cond = 'forall(0:a=0 \/ 0:a=1)'
+
 l0s = {
     'lna': '*y',
     'lrlx': 'atomic_load_explicit(y, memory_order_relaxed)',
@@ -27,23 +30,30 @@ s0s = {
     # Undefined
     'sna': '*x = 1',
 
-    # Unsynchronized
+    # Racy or Unsynchronized
     'srlx': 'atomic_store_explicit(x, 1, memory_order_relaxed)',
     'faddrlx': 'atomic_fetch_add_explicit(x, 1, memory_order_relaxed)',
 
-    # Release Patterns
-    'srel': 'atomic_store_explicit(x, 1, memory_order_release)',
-    'faddrel': 'atomic_fetch_add_explicit(x, 1, memory_order_release)',
+    # Release Patterns with fences
     'frel-srlx': 'atomic_thread_fence(memory_order_release);\n  atomic_store_explicit(x, 1, memory_order_relaxed)',
     'frel-faddrlx': 'atomic_thread_fence(memory_order_release);\n  atomic_fetch_add_explicit(x, 1, memory_order_relaxed)',
     'frel-2srlx': 'atomic_thread_fence(memory_order_release);\n  atomic_store_explicit(x, 1, memory_order_relaxed);\n  atomic_store_explicit(x, 2, memory_order_relaxed)',
+    'frel-2faddrlx': 'atomic_thread_fence(memory_order_release);\n  atomic_fetch_add_explicit(x, 1, memory_order_relaxed);\n  atomic_fetch_add_explicit(x, 1, memory_order_relaxed)',
     'frel-srlx-faddrlx': 'atomic_thread_fence(memory_order_release);\n  atomic_store_explicit(x, 1, memory_order_relaxed);\n  atomic_fetch_add_explicit(x, 1, memory_order_relaxed)',
-    'frel-faddrlx-faddrlx': 'atomic_thread_fence(memory_order_release);\n  atomic_fetch_add_explicit(x, 1, memory_order_relaxed);\n  atomic_fetch_add_explicit(x, 1, memory_order_relaxed)',
+    'frel-faddrlx-srlx': 'atomic_thread_fence(memory_order_release);\n  atomic_fetch_add_explicit(x, 1, memory_order_relaxed);\n  atomic_store_explicit(x, 1, memory_order_relaxed)',
 
-    # Release sequences
-    'srel-srlx': 'atomic_store_explicit(x, 1, memory_order_release);\n  atomic_store_explicit(x, 2, memory_order_relaxed)',
+    # Release Patterns with operations:
+    'srel': 'atomic_store_explicit(x, 1, memory_order_release)',
+    'faddrel': 'atomic_fetch_add_explicit(x, 1, memory_order_release)',
+
+    # Release Patterns with Release Sequences:
     'srel-faddrlx': 'atomic_store_explicit(x, 1, memory_order_release);\n  atomic_fetch_add_explicit(x, 1, memory_order_relaxed)',
     'faddrel-faddrlx': 'atomic_fetch_add_explicit(x, 1, memory_order_release);\n  atomic_fetch_add_explicit(x, 1, memory_order_relaxed)',
+
+    # Release Patterns with Release Sequences: pre-C++17
+    'srel-srlx': 'atomic_store_explicit(x, 1, memory_order_release);\n  atomic_store_explicit(x, 2, memory_order_relaxed)',
+
+    # Not a Release Sequence in C++:
     'faddrel-srlx': 'atomic_fetch_add_explicit(x, 1, memory_order_release);\n  atomic_store_explicit(x, 2, memory_order_relaxed)',
 }
 
@@ -51,15 +61,19 @@ l1s = {
     # Undefined
     'lna': '*x',
 
-    # Unsynchronized
+    # Racy or Undefined
     'lrlx': 'atomic_load_explicit(x, memory_order_relaxed)',
     'faddrlx': 'atomic_fetch_add_explicit(x, 0, memory_order_relaxed)',
 
-    # Acquire Patterns
+    # Acquire Patterns with fences
+    'lrlx-facq': 'atomic_load_explicit(x, memory_order_relaxed);\n  atomic_thread_fence(memory_order_acquire)',
+    'faddrlx-facq': 'atomic_fetch_add_explicit(x, 0, memory_order_relaxed);\n  atomic_thread_fence(memory_order_acquire)',
+
+    # Acquire Patterns with operations
     'lacq': 'atomic_load_explicit(x, memory_order_acquire)',
     'faddacq': 'atomic_fetch_add_explicit(x, 0, memory_order_acquire)',
-    'lrlx-facq': 'atomic_load_explicit(x, memory_order_relaxed);\n  atomic_thread_fence(memory_order_acquire)',
-    'faddrlx-facq': 'atomic_fetch_add_explicit(x, 0, memory_order_relaxed);\n  atomic_thread_fence(memory_order_acquire)',    
+
+    # Not an Acquire Sequence in C++:
     'lrlx-lacq': 'atomic_load_explicit(x, memory_order_relaxed);\n  int c = atomic_load_explicit(x, memory_order_acquire)',
 }
 
@@ -69,28 +83,39 @@ s1s = {
     'faddrlx': 'atomic_fetch_add_explicit(y, 1, memory_order_relaxed)',
 }
 
-for l0, s0, l1, s1 in product(l0s, s0s, l1s, s1s):
+# Release and Acquire patterns:
+release_patterns = ['frel-srlx', 'frel-faddrlx', 'frel-2srlx', 'frel-2faddrlx', 'frel-srlx-faddrlx', 'frel-faddrlx-srlx', 'srel', 'faddrel', 'srel-srel', 'srel-faddrlx', 'faddrel-faddrlx']
+acquire_patterns_no_sequences = ['lrlx-facq', 'faddrlx-facq', 'lacq', 'faddacq']
+acquire_patterns = acquire_patterns_no_sequences + ['lrlx-lacq'] # BUG: lrlx-lacq should not create an acquire pattern
+
+# Pre C++17 release pattenrs include release sequences:
+release_patterns_cpp11 = release_patterns + ['srel-srlx']
+
+# Test check:
+def check(l0, s0, l1, s1, rel, acq):
+    cond = ok_cond
     ub = ''
-    cond = '~exists(0:a=1)'
-    racy_cond = 'forall(0:a=0 \/ 0:a=1)'
-    if l1 in ['lna', 'lrlx', 'faddrlx'] or s0 in ['sna', 'srlx', 'faddrlx', 'faddrel-srlx', 'srel-srlx']:
-        ub = '.undef'
-    if 'lna' not in [l0, l1] and 'sna' not in [s0, s1] and ub == '.undef':
+    # If s1 and l0 do not synchronize with release -> acquire pattern: racy
+    if s0 not in rel or l1 not in acq:
         ub = '.racy'
         cond = racy_cond
-    # Generate a C++11 version with release sequences:
-    if ub == '.undef' and s0 in ['srel-srlx']:
-        cond2 = cond
-        ub2 = ''
-        if l1 in ['lna', 'lrlx', 'faddrlx']:
-            ub2 = '.undef'
-        if 'lna' not in [l0, l1] and 'sna' not in [s0, s1] and ub2 == '':
-            ub2 = '.racy'
-            cond = racy_cond
-        fname = f'lb-{l0}-{s0}-{l1}-{s1}.cpp11{ub2}.litmus'
-        out = shape.format(l0n=l0, s0n=s0, l1n=l1, s1n=s1, l0=l0s[l0], s0=s0s[s0], l1=l1s[l1], s1=s1s[s1], cond=cond2, ub='-cpp11'+ub2.replace('.','-'))
+        # If racy and data-race: undefined behavior:
+        if 'sna' in [s0, s1] or 'lna' in [l0, l1]:
+            ub = '.undef'
+    map = [s0 not in rel, l1 not in acq]
+    return cond, ub, map
+
+for l0, s0, l1, s1 in product(l0s, s0s, l1s, s1s):
+    cond, ub, map = check(l0, s0, l1, s1, release_patterns, acquire_patterns)
+    cond11, ub11, map11 = check(l0, s0, l1, s1, release_patterns_cpp11, acquire_patterns)
+
+    # If C++ < 17 and C++ >=17 condition differs generate two versions of the test, one for C++11 and one for C++17:
+    if cond != cond11 or ub != ub11 or map != map11:
+        fname = f'lb-{l0}-{s0}-{l1}-{s1}.cpp11{ub11}.litmus'
+        out = shape.format(l0n=l0, s0n=s0, l1n=l1, s1n=s1, l0=l0s[l0], s0=s0s[s0], l1=l1s[l1], s1=s1s[s1], cond=cond11, ub='-cpp11' + ub11.replace('.','-'))
         with open(fname, 'w') as f:
             f.write(out)
+
     fname = f'lb-{l0}-{s0}-{l1}-{s1}{ub}.litmus'
     out = shape.format(l0n=l0, s0n=s0, l1n=l1, s1n=s1, l0=l0s[l0], s0=s0s[s0], l1=l1s[l1], s1=s1s[s1], cond=cond, ub=ub.replace('.','-'))
     with open(fname, 'w') as f:
